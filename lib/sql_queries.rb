a8e012e4588e62d41adac7e7ed87b12414cbbdcd
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-13 12:52:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "select color, count(*) from bears group by color order by count limit 1;"
+  "select color, count(*) from bears group by color order by count  desc limit 1 ;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
