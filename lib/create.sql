260c03a1e3cf19fb77e7b51beef6475e6dcfa23f
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-13 12:38:54 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,12 +1,3 @@
-/*Create Table bears (
-  id integer primary key,
-  name text,
-  age integer,
-  gender char(1) check(gender = "M" or gender = "F"),
-  color text,
-  temperament text,
-  alive boolean
-); */
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
